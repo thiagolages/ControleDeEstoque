c3b6af19d4cee5f57cad3d299a671021c9974564
Lusca <lucasf.pereira123@gmail.com>
2018-06-30 18:48:59 -0300

    Banco de dados funcionando e unidade com construtor

--- src/tpfinal/Unidade.java
+++ src/tpfinal/Unidade.java
@@ -1,21 +1,45 @@
 package tpfinal;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Unidade {
-    String nome;
-    String endereco;
-    int coord_end; // coordenadas do endereco p/ calcular o frete
-    ControleDeEstoque estoque; // este estoque precisa ser inicializado dentro do 
-    //construtor com os produtos do banco de dados de acordo com o nome da unidade
+    private String nome;
+    private String endereco;
+    private int id;
+    
     
  
- public Unidade(String nome, String endereco, int coord_end){ // este construtor eh so exemplo
-     // Todas as informacoes da unidade precisam estar no banco de dados
-     // Pensei em fazer um if(nome=="unidade A) inicializa com os dados dela e etc..
-     this.nome = nome;
-     this.endereco = endereco;
-     this.coord_end = coord_end;
- }   
+ public Unidade(int id){
+	 ResultSet rs = null;
+	 Database.connectDatabase();
+	 try
+	 {
+		 rs = Database.query("SELECT * FROM Unity WHERE Unity.unityID = "+id);
+	 }
+	 catch(SQLException err)
+	 {
+		 System.out.println("Ops, erro com a consulta");
+	 }
+	 try
+	 {
+		if(rs.next())
+		{
+			this.id = id;
+			this.nome = rs.getString("unityName");
+			this.endereco = rs.getString("address");
+			System.out.println(this.nome);
+			System.out.println(this.endereco);
+		}
+		else
+		{
+			System.out.println("Erro ao encontrar resultados");
+		}
+	 }
+	 catch(SQLException err)
+	 {
+		 System.out.println("Erro ao encontrar resultados");
+	 }
+ }
     
 }
 
